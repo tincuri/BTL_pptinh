8e42af68972d1d84781349e45e226381c712a32e
Le Duc Minh <minhle428to@gmail.com>
2025-02-22 15:12:54 +0000

    complete the thing, need some cleanup

--- C_code/dcel/scan.c
+++ C_code/dcel/scan.c
@@ -4,8 +4,18 @@
 #include <ctype.h>
 
 #include "dcel.h"
+#include "misc.h"
 
-struct dcel_vertex *scan_point(FILE *pos){
+struct point *scan_point(FILE *pos){
+  double x, y;
+  char ch;
+
+  while (isspace(ch = fgetc(pos)));  // Skip whitespace
+  ungetc(ch, pos);      // Put back the non-space character
+  if (fscanf(pos, "(%lf,%lf)", &x, &y) == 2) /* be sure that it work */
+  return new_point(x, y);
+}
+struct dcel_vertex *scan_vertex(FILE *pos){
   double x, y;
   char ch;
 
--- C_code/dcel/scan.h
+++ C_code/dcel/scan.h
@@ -2,7 +2,8 @@
 #define scan_h
 
 /*         functions prototype         */
-struct dcel_vertex *scan_point(FILE *pos);
+struct point *scan_point(FILE *pos);
+struct dcel_vertex *scan_vertex(FILE *pos);
 struct dcel_edge *scan_edge(FILE *pos);
 
 #endif /* ifndef scan_h */
