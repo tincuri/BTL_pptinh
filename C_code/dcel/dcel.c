26b0cd67084e378be7c6178a6bc45cbd2c54587e
Le Duc Minh <minhle428to@gmail.com>
2025-03-17 15:47:43 +0700

    clean up and create a makefile

--- C_code/dcel/dcel.c
+++ C_code/dcel/dcel.c
@@ -1,16 +1,16 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "dcel.h"
 #include "misc.h"
 
 /* create a new vertex and return it */
-struct dcel_vertex *new_vertex(double x, double y){
+struct dcel_vertex *new_vertex(double x, double y) {
   struct dcel_vertex *new;
   new = malloc(sizeof(struct dcel_vertex));
   new->x = x;
   new->y = y;
-  struct node *head, *z;
   new->head = malloc(sizeof(struct node));
   new->head->next = NULL;
   return new;
@@ -18,7 +18,7 @@ struct dcel_vertex *new_vertex(double x, double y){
 
 /* create a new half_edge and its twin */
 /* des for destination, org for origin, h_edge for half edge */
-struct dcel_edge *new_edge(struct dcel_vertex *org, struct dcel_vertex *des){
+struct dcel_edge *new_edge(struct dcel_vertex *org, struct dcel_vertex *des) {
   struct dcel_edge *h_edge, *twin;
 
   h_edge = malloc(sizeof(struct dcel_edge));
@@ -39,7 +39,7 @@ struct dcel_edge *new_edge(struct dcel_vertex *org, struct dcel_vertex *des){
   return h_edge;
 }
 
-void next_prev(struct dcel_edge *first, struct dcel_edge *next){
+void next_prev(struct dcel_edge *first, struct dcel_edge *next) {
   first->Next = next;
   next->Prev = first;
 }
--- C_code/dcel/dcel.h
+++ C_code/dcel/dcel.h
@@ -3,13 +3,13 @@
 
 /*          Structure           */
 
-enum face {OUTSIDE, TRIANGLE, NONE};
+enum face { OUTSIDE, TRIANGLE, NONE };
 
 struct dcel_vertex {
   double x;
   double y;
-  int index; /* the index of the vertex in the vertex_list */
-  struct node *head;/* head of the linked list */
+  int index;         /* the index of the vertex in the vertex_list */
+  struct node *head; /* head of the linked list */
 };
 
 struct dcel_edge {
@@ -18,13 +18,14 @@ struct dcel_edge {
   struct dcel_face *IncidentFace;
   struct dcel_edge *Next;
   struct dcel_edge *Prev;
-  bool connected; /* boolean to check if a graph connection has been added through the edge*/
+  bool connected; /* boolean to check if a graph connection has been added
+                     through the edge*/
 };
 
 struct dcel_face {
   struct dcel_edge *incidented_edge; /* an edge that has it as IncidentFace */
   enum face type; /* if its a polygon face of a triangle face */
-  int index; /* index of the face in the graph list */
+  int index;      /* index of the face in the graph list */
 };
 
 /*               Function prototype             */
@@ -33,23 +34,22 @@ struct dcel_vertex *new_vertex(double x, double y);
 struct dcel_edge *new_edge(struct dcel_vertex *org, struct dcel_vertex *des);
 void next_prev(struct dcel_edge *first, struct dcel_edge *next);
 
-
 /*            Global variable              */
 
-extern int vertex_count; /* number of vertex of the polygon */
-extern int hole_count; /* number of holes */
-extern int face_count; /* number of face */
+extern int vertex_count;                 /* number of vertex of the polygon */
+extern int hole_count;                   /* number of holes */
+extern int face_count;                   /* number of face */
 extern struct dcel_vertex **vertex_list; /* pointer to a list of vertex */
 extern struct dcel_face **face_list;
 extern struct graph_node **face_graph; /* dual graph of the polygon */
-extern struct dcel_vertex *first; /* first vertex */
-extern struct dcel_edge *f_edge; /* first edge */
-extern struct dcel_edge **pi_path; /* a stack of h_edge for pi_pat */
+extern struct dcel_vertex *first;      /* first vertex */
+extern struct dcel_edge *f_edge;       /* first edge */
+extern struct dcel_edge **pi_path;     /* a stack of h_edge for pi_pat */
 extern int *dfsval;
 /* face index of the 2 points */
 extern int face1;
 extern int face2;
 /* variable for stack */
-extern struct node *head, *z; 
+extern struct node *head, *z;
 
 #endif /* ifndef h_dcel */
